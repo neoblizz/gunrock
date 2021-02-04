bf6a96790631908269113087575ca17ac655e470
agalup <lupinska.agnieszka@gmail.com>
2021-02-04 12:13:58 -0800

    Update compilation

--- cmake/FindFAISS.cmake
+++ cmake/FindFAISS.cmake
@@ -3,12 +3,15 @@
 # ------------------------------------------------------------------------
 FIND_LIBRARY( FAISS_LIBRARY
               NAMES faiss
-              PATHS ../faiss /lib /usr/lib /usr/lib64 /usr/local/lib)
+              PATHS $ENV{FAISS_LIBRARY} )
 
 FIND_PATH(  FAISS_INCLUDE_DIR
-            NAMES faiss/gpu/GpuIndexFlat.h
-            PATHS /usr/include ../)
+            NAMES faiss
+            PATHS $ENV{FAISS_INCLUDE_DIR} )
 
 IF (FAISS_LIBRARY)
     ADD_DEFINITIONS( -DFAISS_FOUND=1)
+    INCLUDE_DIRECTORIES(${FAISS_INCLUDE_DIR})
+    LINK_DIRECTORIES(${FAISS_INCLUDE_DIR}/build/faiss)
+    LINK_DIRECTORIES(${FAISS_INCLUDE_DIR}/build/faiss/python)
 ENDIF (FAISS_LIBRARY)
